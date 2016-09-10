1d3f072fa8bbfc209544c85b2301d015da59dc64
Tuan Bui <Tuan Bui>
2016-09-10 16:09:56 +0700

    update install file

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 URXVT_DIR=/usr/lib/urxvt/perl/
+FILES=("Xresources" "clipboard")
 
 function die () {
     echo "${@}"
@@ -14,7 +15,13 @@ cat <<EOF
 -----------------------------
 
 EOF
+# MOVE THE FILES
+for i in ${FILES[*]}; do
+    echo "Downloading $i"; echo
+    wget -c https://github.com/tbui/urxvt-config/raw/master/$i \
+      || die "Fail to download ${i}"
+done
 
-mv .Xresources $HOME || die "error moving .Xresources"
+mv Xresources $HOME/.Xresources || die "error moving .Xresources"
 # install clipboard
 sudo mv clipboard $URXVT_DIR || die "couldn't install clipboard"
